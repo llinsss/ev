c11b578eff5dd971cb053f522566eff27edfe0d5
llins <llinsomoudu@gmail.com>
2025-05-10 14:29:52 +0100

    exp

--- src/App.js
+++ src/App.js
@@ -4,3 +4,16 @@ import TicketSystem from './components/TicketSystem';
 
 function App() {
   return (
+    <div className="App">
+      <header>
+        <h1>Event Ticketing App</h1>
+        <WalletButton />
+      </header>
+      <main>
+        <TicketSystem />
+      </main>
+    </div>
+  );
+}
+
+export default App;
