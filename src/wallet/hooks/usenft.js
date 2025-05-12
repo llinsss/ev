1a9d59bb360c151a542052be2bed8a89db9d7bfc
llins <llinsomoudu@gmail.com>
2025-05-12 10:35:00 +0100

    nft

---
+++ src/wallet/hooks/usenft.js
@@ -0,0 +1,8 @@
+export const useNFTTickets = (contractAddress) => {
+    const { contract } = useContract(contractAddress, nftAbi);
+    const [nfts, setNFTs] = useState([]);
+    
+    const fetchTickets = async (owner) => {
+        const balance = await contract.balanceOf(owner);
+        // ... fetch token URIs
+    };
