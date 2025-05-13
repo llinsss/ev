eef7fea4a8b0fb6f001d4610ec3cb99b97261588
llins <llinsomoudu@gmail.com>
2025-05-13 16:55:56 +0100

    utils

---
+++ src/utils/aiHelpers.js
@@ -0,0 +1,7 @@
+export const fetchAISuggestions = async (eventTitle) => {
+    const response = await fetch('https://api.openai.com/v1/completions', {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${process.env.OPENAI_KEY}`,
+        'Content-Type': 'application/json'
+      },
