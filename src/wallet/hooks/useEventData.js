3ef1c96363232b2c820b4a1ddeb7838bd29fe5b4
llins <llinsomoudu@gmail.com>
2025-05-13 16:58:31 +0100

    socket

--- src/wallet/hooks/useEventData.js
+++ src/wallet/hooks/useEventData.js
@@ -2,4 +2,8 @@ useEffect(() => {
     const socket = new WebSocket(process.env.WS_URL);
     socket.onmessage = (event) => {
       const data = JSON.parse(event.data);
+      setLiveAttendees(prev => [...prev, data.newAttendee]);
+  };
+  return () => socket.close();
+}, []);
       
